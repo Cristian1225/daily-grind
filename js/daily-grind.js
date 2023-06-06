373d8522e4b2220f5bafa0c94169cf9b6acb4d4f
Cristian1225 <cristianoreyes234@gmail.com>
2023-06-05 23:21:11 -0700

    updated index

--- js/daily-grind.js
+++ js/daily-grind.js
@@ -1,70 +1,75 @@
-/* 
-pic - image src 
-alt - alt tag for the page 
-color - color to match image 
-description - description of coffee
-name - name of coffee
-day  - day of the week 
+/*
+
+    Here are the items we believe we need to produce for each day's coffee
+
+    pic - image src
+    alt - the alt tag for the image
+    color - color to match image
+    desc - description of coffee
+    name - name of the coffee
+    day - the day of the week
 
 */
+
+function coffeeTemplate(coffee){
+
+
+return 
+  <p>
+             <img src="${coffee.pic}" alt="${coffee.alt}" id="coffee" />
+            <strong class="feature">${coffee.day}'s Coffee Special:</strong> ${coffee.day}'s daily coffee special is <strong class="feature">${coffee.name}</strong>, ${coffee.desc}</p>
+
+  ;
+
+
+
+
+
+
+
+}
+
 let myDate = new Date();
+
+let coffee = "";
+
 let today = myDate.getDay();
 
-function coffeTemplate(coffee) {
+//today = 3;
 
- /*
-return `
-    <p>
-    <img src="${coffee.image}" alt="${coffee.alt}" id="coffee" />
-    <strong class="feature">${coffee.day} Coffee Special:</strong> ${coffee.day} daily coffee special is <strong class="feature">${coffee.name}</strong>, ${coffee.desc}</p>
-    <p>
+switch(today){
 
-    `; 
-    */
-}
-
-return `<p>
-<img src="${coffee.pic}" alt="Our Pumpkin Spice Latte tastes great on a Fall Day!" id="coffee" />
-<strong class="feature">Monday's Coffee Special:</strong> Monday's daily coffee special is <strong class="feature">${coffee.name}</strong>, which makes us wish it was always Fall, as this is one of our top sellers!</p>
-`;
-
-switch(today) {
     case 1:
         today = "Monday";
-        coffee = { 
-            name: "Bubble Tea", 
-            color: "Pink ",
-            image: "images/bubble-tea.jpg", 
-            alt: "A picture of a bubble tea",
-            day: "Wednesday",
-            desc: `I like me some bubble tea`
-        }
-        
-    case 2: 
-       today = "Tuesday";        
-       break;
-    case 3: 
-       today = "Wednesday";        
-        break;
+        coffee = {
+          name:"Bubble Tea",
+          color: "pink",
+          pic: "images/bubble-tea.jpg",
+          alt: "A picture of a bubble tea",
+          day: "Monday",
+          desc: I like me some bubble tea!
+        };
 
-    default:
-        today = "Not sure what day it is";
+
+      break;
+
+      case 2:
+        today = "Tuesday";
+      break;
+
+    case 3:
+        today = "Wednesday";
+    break;
+
+      default:
+          today = "Not sure what day it is!";
         alert("falling into alert!");
+
 }
 
-alert(coffeeTemplate(coffee));
+//alert(coffeeTemplate(coffee));
 
-document.getElementById("coffee-cup").innerHTML = coffeTemplate(coffee); 
+document.getElementById("coffee-cup").innerHTML = coffeeTemplate(coffee);
 
-// How to set an object 
-coffee = { 
-    name: "Bubble Tea", 
-    color: "Pink ",
-    image: "images/bubble-tea.jpg", 
-    alt: "A picture of a bubble tea",
-    day: "Wednesday",
-    desc: `I like me some bubble tea`
-}
-
-//alert(today)
+//alert(today);
 console.log(coffee);
